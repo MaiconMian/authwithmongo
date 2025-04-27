7ec624b14162639d143b81d2a2711cf71b3352e4
MaiconMian <maicon.mian@sou.unifal-mg.edu.br>
2025-04-27 18:26:57 -0300

    Adding initial files

---
+++ src/main/java/io/github/maiconmian/authwithmongo/AuthwithmongoApplication.java
@@ -0,0 +1,13 @@
+package io.github.maiconmian.authwithmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthwithmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthwithmongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/maiconmian/authwithmongo/AuthwithmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.maiconmian.authwithmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthwithmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
